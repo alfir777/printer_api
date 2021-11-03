5d325a264aa34f6ad9d0b605ab94876910458e8b
Alfir Tyakin <alfir777@rambler.ru>
2021-11-03 19:26:20 +0500

    custom admin

--- config/core/admin.py
+++ config/core/admin.py
@@ -11,20 +11,26 @@ class PrinterAdminForm(forms.ModelForm):
 
 
 class PrinterAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('General', {
+            'fields': (
+                'name', 'api_key', 'check_type', 'point_id'
+            )
+        }),
+    )
     form = PrinterAdminForm
     save_as = True
     save_on_top = True
-    list_display = ('id', 'name', 'api_key', 'check_type', 'point_id')
-
-
-# class CheckAdminForm(forms.ModelForm):
-#     class Meta:
-#         model = Printer
-#         fields = '__all__'
+    list_display = ('id', 'name', 'check_type', 'point_id')
+    list_display_links = ('id', 'name')
 
 
 class CheckAdmin(admin.ModelAdmin):
-    list_filter = ('printer_id', 'type')
+    save_as = True
+    save_on_top = True
+    list_display = ('id', '__str__', 'printer_id', 'type', 'order', 'status', 'pdf_file')
+    list_display_links = ('id', '__str__')
+    list_filter = ('printer_id', 'type', 'status')
 
 
 admin.site.register(Printer, PrinterAdmin)
